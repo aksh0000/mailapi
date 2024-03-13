6f50ec367687b51ff14c7282c7550d87774df724
aksh0000 <ruthlessdestroyer085@gmail.com>
2024-03-13 09:15:38 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express=require('express');
+const app=express();
+const cors=require('cors');
+const nodemailer=require('nodemailer')
+const bodyParser = require('body-parser')
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/',(req,res)=>{
+    res.write("This shit is working fine!");
+    res.end();
+})
+app.post('/api',(req,res)=>{
+    
+    let message_data=req.body;
+    console.log(message_data);
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'ruthlessdestroyer085@gmail.com', // Your email address
+            pass: 'vhjh zzeo mkwr pqvp' // Your password
+        }
+    });
+    
+    // Define email options
+    let mailOptions = {
+        from: 'ruthlessdestroyer085@gmail.com', // Sender address
+        to: 'bdevi2472@gmail.com', // List of recipients
+        subject: 'Test Email', // Subject line
+        text: req.body.message // Plain text body
+        // You can also include HTML content as 'html' property
+    };
+    
+    // Send email
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error occurred: ', error);
+        } else {
+            console.log('Email sent successfully!');
+            console.log('Message ID: ', info.messageId);
+            res.status(200).json({data:message_data})
+        }
+    });
+
+
+});
+
+app.listen(8000);
+
